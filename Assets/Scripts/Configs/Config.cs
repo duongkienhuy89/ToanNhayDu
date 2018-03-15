ff8fce1b9616f716006cfa55ff153841f384cc85
Huy Vu <dinhthitampro@gmail.com>
2018-03-16 01:35:09 +0700

    up len styore 3

--- Assets/Scripts/Configs/Config.cs
+++ Assets/Scripts/Configs/Config.cs
@@ -17,9 +17,9 @@ public class Config  {
 #if UNITY_ANDROID
 
 
-	public static string adsInIDGameOver = "ca-app-pub-3940256099942544/1033173712";
+    public static string adsInIDGameOver = "ca-app-pub-5148482490300491/5550042834";
 
-	public static string adsInIDBanner = "ca-app-pub-3940256099942544/6300978111";
+    public static string adsInIDBanner = "ca-app-pub-5148482490300491/1251705179";
                         
 
 #endif
